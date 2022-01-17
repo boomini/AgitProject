324f6a2ee15e953b1999acaee3854c1247b99725
임지환 <top1h@naver.com>
2022-01-17 16:26:45 +0900

    Create intro

---
+++ frontend/src/views/intro/intro.vue
@@ -0,0 +1,28 @@
+<template>
+    <div data-aos="fade-in">이거는</div>
+</template>
+
+
+<script>
+import { onMounted } from 'vue'
+import { useStore } from 'vuex'
+
+
+
+export default {
+  name: 'Intro',
+  setup () {
+    const store = useStore()
+
+    // 페이지 진입시 불리는 훅
+    onMounted (() => {
+      store.commit('root/setMenuActiveMenuName', 'intro')
+
+    })
+  }
+}
+</script>
+
+<style>
+
+</style>
