03d649319881b4ad52c22cfea3cf59008f02a3b4
임지환 <top1h@naver.com>
2022-02-06 14:04:37 +0900

    Create schedule

--- frontend/src/views/room/room.vue
+++ frontend/src/views/room/room.vue
@@ -109,6 +109,7 @@
   <!-- 일정 추가 다이얼로그 -->
   <create-schedule-dialog
     :open="state.createScheduleDialogOpen"
+    :info="state.team"
     @closeCreateScheduleDialog="onCloseCreateScheduleDialog"/>
 
   <!-- 사진 추가 다이얼로그 -->
@@ -252,6 +253,7 @@ export default {
       state.team.teamDescription = route.params.roomDescription
       state.team.teamPicture = route.params.roomPicture
       console.log(state.team.teamName)
+
 
       // 이번 달 달력 가져오기
       // store.commit()
