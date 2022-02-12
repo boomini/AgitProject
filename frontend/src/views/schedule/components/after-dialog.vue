ffe50699902385e9c666d4cdf0544f0bd65d4e8a
임지환 <top1h@naver.com>
2022-02-12 16:35:48 +0900

    Edit speechbubble

--- frontend/src/views/schedule/components/after-dialog.vue
+++ frontend/src/views/schedule/components/after-dialog.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div v-if="info">
     <el-dialog custom-class="after-dialog" v-model="state.dialogVisible" title="다음 약속" width="30%" style="margin: 50%">
       <span>
       팀 이름: {{ info.teamName }} <br>
@@ -8,6 +8,20 @@
       D-day: {{ info.dday }}일 <br>
       약속 제목: {{ info.eventTitle }} <br>
       약속 내용: {{ info.eventContent }}
+      </span>
+      <template #footer>
+        <span class="dialog-footer">
+          <el-button type="primary" @click="handleClose()"
+            >Confirm</el-button
+          >
+        </span>
+      </template>
+    </el-dialog>
+  </div>
+  <div v-else-if="info == undefined">
+    <el-dialog custom-class="before-dialog" v-model="state.dialogVisible" title="다음 약속" width="30%" style="margin: 50%">
+      <span>
+      일정을 추가해주세요!!
       </span>
       <template #footer>
         <span class="dialog-footer">
