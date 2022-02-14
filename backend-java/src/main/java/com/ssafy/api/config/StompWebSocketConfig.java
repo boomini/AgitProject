4b50547b21c7f27a3ed0ce56bb01ce26cb38f809
yunhy96 <yunhy96@naver.com>
2022-02-14 15:33:41 +0900

    change

--- backend-java/src/main/java/com/ssafy/api/config/StompWebSocketConfig.java
+++ backend-java/src/main/java/com/ssafy/api/config/StompWebSocketConfig.java
@@ -8,17 +8,23 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 
 @Configuration
 @EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+public class StompWebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
+    //endpoint를 /stomp로 하고, allowedOrigins를 "*"로 하면 페이지에서
+    //Get /info 404 Error가 발생한다. 그래서 아래와 같이 2개의 계층으로 분리하고
+    //origins를 개발 도메인으로 변경하니 잘 동작하였다.
+    //이유는 왜 그런지 아직 찾지 못함
     @Override
-    //connetion 시 Cors 허용
-    public void registerStompEndpoints(StompEndpointRegistry registry){
-        registry.addEndpoint("/example").withSockJS();
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/stomp/chat")
+                .setAllowedOrigins("http://localhost:8085")
+                .withSockJS();
     }
 
+    /*어플리케이션 내부에서 사용할 path를 지정할 수 있음*/
     @Override
-    public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.setApplicationDestinationPrefixes("/test");
-        config.enableSimpleBroker("/topic", "/queue");
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setApplicationDestinationPrefixes("/pub");
+        registry.enableSimpleBroker("/sub");
     }
 }
